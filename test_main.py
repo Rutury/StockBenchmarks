7d3ada49c8ddbec63a6b499ad4bb33be8bdecadb
Rutury <yury.filyanin@yandex.ru>
2024-11-15 10:40:03 +0000

    added more tests

--- test_main.py
+++ test_main.py
@@ -1,11 +1,12 @@
 import main
 import pytest
 
-dates = ["2024-11-14", "2024-11-10", "6666-66-66"]
+dates = ["2024-11-14", "2024-11-10", "6666-06-06", "1111-11-11", "2022-03-23", "2022-03-24"]
 prices = {
-    "IMOEX" : [2697.47, 2734.56, -1],
-    "SBER" : [249.52, 255.98, -1],
-    "LQDT" : [1.5224, 1.5185, -1]
+    "IMOEX" : [2697.47, 2734.56, -1, -1, -1, 2578.51],
+    "SBER" : [249.52, 255.98, -1, -1, -1, 136.24],
+    "LQDT" : [1.5224, 1.5185, -1, -1, -1, -1]#,
+    # "MyIncorrectTicket" : [-1, -1, -1, -1, -1, -1]
 }
 
 securities = {
@@ -14,9 +15,9 @@ securities = {
 }
 
 @pytest.mark.parametrize("ticker, date", [
-    (ticker, date) 
-    for ticker in prices.keys()
+    (ticker, date)
     for date in dates
+    for ticker in prices.keys()
 ])
 def test_tickers(ticker, date):
     idx = dates.index(date)
